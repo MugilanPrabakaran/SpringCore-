e6b38471e3e9a4de7d05d54dae26024a8d1a3af8
MugilanPrabakaran <mugilsivasu@gmail.com>
2022-12-14 19:41:20 +0530

    updating @Autowired

--- src/AutowiredinSpring/Human.java
+++ src/AutowiredinSpring/Human.java
@@ -1,11 +1,20 @@
 package AutowiredinSpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Human {
     private  Heart heart;
 
     public void setHeart(Heart heart) {
         this.heart = heart;
     }
+@Autowired
+    public Human(Heart heart) {
+        this.heart = heart;
+    }
+public Human(){
+
+}
     public void startPumping(){
         if (heart != null) {
             heart.pump();
