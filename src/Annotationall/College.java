883f7065feccf8693e0c9218df4d0ebb45b5141b
Mugilan Prabakaran <110663846+MugilanPrabakaran@users.noreply.github.com>
2022-12-21 11:30:19 +0530

    Delete src/Annotationall directory

--- src/Annotationall/College.java
+++
@@ -1,8 +0,0 @@
-package Annotationall;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-@Component("collegeBean")
-public class College {
-}
