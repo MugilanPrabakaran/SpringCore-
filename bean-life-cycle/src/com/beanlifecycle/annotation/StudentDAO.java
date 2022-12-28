0ca42a42b9619479ed5b5d527b14d9d5f64eadcd
MugilanPrabakaran <mugilsivasu@gmail.com>
2022-12-28 17:11:49 +0530

    Updating JDBC with SQL query

--- bean-life-cycle/src/com/beanlifecycle/annotation/StudentDAO.java
+++ bean-life-cycle/src/com/beanlifecycle/annotation/StudentDAO.java
@@ -1,9 +1,6 @@
 package com.beanlifecycle.annotation;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class StudentDAO {
     private String driver = "com.mysql.jdbc.Driver";
@@ -19,11 +16,28 @@ public class StudentDAO {
         //execute query
         Statement state =  connection.createStatement();
         ResultSet rs = state.executeQuery("Select * from studentdetails");
+        System.out.println("Retrieving all student records");
 
-        while (rs.next()) {
+        while (rs.next())/* next() means run loop until empty space*/{
             String studid = rs.getString(1);
             System.out.println("Student Id is : "+studid );
         }
+        //Closing the connection
+        connection.close();
 
     }
+    public void deleterec(int studid) throws Exception {
+        //load driver
+        Class.forName(driver);
+        //Get a connection
+        Connection connection = DriverManager.getConnection(url, userName, password);
+        //execute query
+        Statement stmt =  connection.createStatement();
+        //Deleting the particular student details
+        stmt.executeUpdate("Delete from studentdetails where studid ="+studid);
+        System.out.println("Deleted the Student Info "+studid);
+        //closing the connection
+        connection.close();
+    }
+
 }
